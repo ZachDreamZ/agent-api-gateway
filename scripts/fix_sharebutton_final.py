5f5c67a0ae02e057ddfdef602c46ed5f0a83bf8d
ZachDreamZ <xxtheshadowcarft@gmail.com>
2026-07-23 14:42:07 +0800

    chore: Remove temporary one-off Python scripts

--- scripts/fix_sharebutton_final.py
+++
@@ -1,3 +0,0 @@
-import sys
-with open(r'D:\micro-saas-agent-api\src\dashboard\src\components\ShareButton.tsx', 'w', encoding='utf-8') as f:
-    f.write('import React, { useState } from ''react'';' + chr(10))
